c69e71299e7dae242fa6faa99f7c59c7bce7b5a9
Matt Bergstrom <mbergstrom@embtrak.com>
2025-07-17 08:46:03 -0400

    WASM app and basic rendering

---
+++ PegSolitaire.Components/ElementSizeGetter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.JSInterop;
+
+namespace PegSolitaire.Components;
+
+public class ElementSizeGetter : IAsyncDisposable
+{
+    private class Size
+    {
+        public double width { get; set; }
+        public double height { get; set; }
+    }
+
+    private readonly Lazy<Task<IJSObjectReference>> _ModuleTask;
+
+    public ElementSizeGetter(IJSRuntime jsRuntime)
+    {
+        _ModuleTask = new(() =>
+            jsRuntime.InvokeAsync<IJSObjectReference>("import",
+                "./_content/PegSolitaire.Components/scripts/domInterop.js").AsTask());
+    }
+
+    public async Task<(double? Width, double? Height)> GetElementSizeAsync(string id)
+    {
+        var module = await _ModuleTask.Value;
+        var size = await module.InvokeAsync<Size>("getElementSize", id);
+        return (size?.width, size?.height);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_ModuleTask.IsValueCreated)
+        {
+            var module = await _ModuleTask.Value;
+            await module.DisposeAsync();
+        }
+    }
+}
---
+++ PegSolitaire.Components/RegistrationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using BlazorObservers.ObserverLibrary.DI;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PegSolitaire.Components;
+
+public static class RegistrationExtensions
+{
+    public static IServiceCollection AddPegSolitaireComponents(this IServiceCollection services)
+    {
+        services.AddResizeObserverService();
+        return services;
+    }
+}
